5da1e71eff2e6e53e55b734623386bac27f11929
michele.scarlato <michele.scarlato@gmail.com>
2020-05-29 21:17:17 +0900

    ok

--- tests/TPS_bash_script.sh
+++ tests/TPS_bash_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nodes=4
+nodes=3
 x=1
 COUNT=50
 
@@ -9,7 +9,7 @@ start=`date +%s.%N`
 while [ $x -le $COUNT ]
 do
   node script_node1.js
-  node script_node2.js 
+  node script_node2.js
   node script_node3.js
   echo "Executed $x times"
   x=$(( $x + 1 ))
@@ -18,9 +18,10 @@ end=`date +%s.%N`
 #timestamp
 runtime=$( echo "$end - $start" | bc -l )
 RESULT=$(echo "$runtime/($COUNT*3)" | bc -l)
-echo "Number of nodes: "$nodes
-echo "Elapsed time: "$runtime
+touch TPS.log
+echo "Number of nodes: "$nodes >> TPS.log
+echo "Elapsed time: "$runtime >> TPS.log
 TRANSACTIONS=$(($COUNT*3))
-echo "Number of transactions: "$TRANSACTIONS
-echo "Throughput in terms of transactions per second: "$RESULT
-echo " "
+echo "Number of transactions: "$TRANSACTIONS >> TPS.log
+echo "Throughput in terms of transactions per second: "$RESULT >> TPS.log
+echo " " >> TPS.log
