b60803ce4c769d4e82d5ecca5f42afbce0c63e13
michelescarlato <michele.scarlato@gmail.com>
2020-11-19 16:33:49 +0900

    with timestamp

--- tests/script_node2.js
+++ tests/script_node2.js
@@ -3,8 +3,8 @@ const driver = require('bigchaindb-driver')
 const API_PATH = 'http://128.163.232.68:9984/api/v1/'
 
 
-//var date = new Date();
-//var timestamp = date.getTime();
+var date = new Date();
+var timestamp = date.getTime();
 
 let fs = require('fs')
 
@@ -17,7 +17,7 @@ let bob_privkey = fs.readFileSync(process.cwd() + "/" + filename_privkey).toStri
 //console.log("Private key " + bob_privkey)
 
 
-const assetdata = { "What is your country and city of residence?": "France", "City": "Nice", "Gender": "Male", "Age": "25-49", "overnight": true, "how many nights": "3", "did you from:": "Home", "Purpose": [ "Holiday" ], "how many:": "Alone", "primary transport:": [ "Motorcycle" ], "method transport here:": [ "Motorcycle" ], "first time here:": true, "Interesting Features": [ "Beach facilities" ], "kind of accomodation:": "Hotel / Resort / Motel", "spend per day:": "25 - 50 €", "expenses:": "Within what was planned", "disability or reduced mobility:": false, "special needs:": "Agree", "sustainability efforts": false, "Overall Satisfaction": "Agree", "Disability considerations": "Slightly Agree", "Highest Degree": "Bachelor’s degree", "Professional Status": "Self-employed", "Annual Household": "Medium (from 30.000 to 60.000 euros)" }//, "Timestamp" : ""+timestamp+"" , "Survey Type": "visitors" }
+const assetdata = { "What is your country and city of residence?": "France", "City": "Nice", "Gender": "Male", "Age": "25-49", "overnight": true, "how many nights": "3", "did you from:": "Home", "Purpose": [ "Holiday" ], "how many:": "Alone", "primary transport:": [ "Motorcycle" ], "method transport here:": [ "Motorcycle" ], "first time here:": true, "Interesting Features": [ "Beach facilities" ], "kind of accomodation:": "Hotel / Resort / Motel", "spend per day:": "25 - 50 €", "expenses:": "Within what was planned", "disability or reduced mobility:": false, "special needs:": "Agree", "sustainability efforts": false, "Overall Satisfaction": "Agree", "Disability considerations": "Slightly Agree", "Highest Degree": "Bachelor’s degree", "Professional Status": "Self-employed", "Annual Household": "Medium (from 30.000 to 60.000 euros)", "Timestamp" : ""+timestamp+""}//, "Survey Type": "visitors" }
 console.log(assetdata)
 
 //const assetdata = { "Vote": "White" }
