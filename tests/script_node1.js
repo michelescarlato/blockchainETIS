ed9f40f9e8ad97baa21328d6bbd6a868c8d78495
michelescarlato <michele.scarlato@gmail.com>
2020-11-19 16:03:53 +0900

    trying to fix the assetdata part

--- tests/script_node1.js
+++ tests/script_node1.js
@@ -21,8 +21,8 @@ let alice_privkey = fs.readFileSync(process.cwd() + "/" + filename_privkey).toSt
 
 //const assetdata = { "What is your country and city of residence?": "Spain", "City": "Sevilla", "Gender": "Female", "Age": "25-49", "overnight": false, "If No, how many hours:": "5", "did you from:": "Home", "Purpose": [ "Visiting relatives and friends" ], "how many:": "Alone", "primary transport:": [ "Car" ], "method transport here:": [ "Walk" ], "first time here:": false, "If No, how many times:": "3", "When last visit:": "Last 12 months", "Interesting Features": [ "A particular event", //"Peace and quiet", "Scenery and countryside", "Historic interest" ], "kind of accomodation:": "Friend's / Relative's home", "spend per day:": "25 - 50 €", "expenses:": "Within what was planned", "disability or reduced mobility:": false, "special needs:": "Agree", "sustainability efforts": true, "initiatives aware:": "Differentiate garbage collection", "Overall Satisfaction": "Agree", "Disability considerations": "Agree", "Highest Degree": "Master’s degree", "Professional Status": "Part-time //employed for wages", "Annual Household": "Low (under 30.000 euros)", "Timestamp" : ""+timestamp+"", "Survey Type": "visitors" }
 
-const assetdata = { "Vote": "White" }
-
+//const assetdata = { "Vote": "White" }
+const assetdata = { "What is your country and city of residence?": "France" }
 
 const metadata = {"what": "My first bigchaindb transaction TEST"}
 // Construct a transaction payload
